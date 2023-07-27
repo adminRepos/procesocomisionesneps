339da5b2909ccec1ae6c5bf729a89f16dfacb9aa
Jesus Bernardo Mendoza Romero <jmendoza@qvision.us>
2023-07-27 16:13:25 -0500

    actualizar front solucion error css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export const environment = {
   // apiUrl: 'http://localhost:5000',
   // apiUrl: 'http://34.121.2.93:8080',
   apiUrl: 'https://back.lupaapsalud.com',
   
 };
-=======
-export const environment = {
-  // apiUrl: 'http://localhost:5000',
-  // apiUrl: 'http://34.121.2.93:8080',
-  apiUrl: 'https://back.lupaapsalud.com',
-  
-};
->>>>>>> fd73b7451bed1dc8c5b9180dfe1d8b4fac6b59b8
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -6,12 +5,3 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-=======
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
->>>>>>> fd73b7451bed1dc8c5b9180dfe1d8b4fac6b59b8
